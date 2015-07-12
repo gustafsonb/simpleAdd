c426f2359d81c7cc98efe3b4eacd027fe32bd218
Brad Gustafson <gustafsonb37@gmail.com>
2015-07-12 15:35:33 -0500

    initial

---
+++ SimpleAdd.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SimpleAdd {
+	public static void main( String[] args ){
+		Scanner in = new Scanner(System.in);
+		
+		
+		int a, b, c;
+		
+        while(true){
+            System.out.println("Again?????");
+		
+		System.out.println("First number");
+		a = in.nextInt();
+		
+		System.out.println("Second number");
+		b = in.nextInt();
+		
+		
+		c = a + b;
+		
+		System.out.println("Sum of inputs  "+c);
+		
+		
+                    
+                    if(){
+                    }
+		}
+			
+				
+        }
+			
+}
+	
